34516736c5a5382ad9e39fb58ee5884a4f61af19
drphizzle <otuphil60@gmail.com>
2025-01-30 02:59:27 +0000

    adding all edits

--- script.js
+++ script.js
@@ -94,3 +94,21 @@ dislikButton.addEventListener('click', () => {
     }
 });
 updateLifeFact();
+
+// adding click event to the menu-icon
+
+
+    const menuIcon = document.querySelector('.menu-icon');
+    const navMenu = document.querySelector('.ul-menu');
+    
+    menuIcon.addEventListener('click', () => {
+        navMenu.classList.toggle("active");
+    });
+
+    // close menu after clicking on a link
+    document.querySelectorAll('.nav-menu a').forEach(link => {
+        link.addEventListener('click', () => {
+            navMenu.classList.remove("active");
+        });
+
+    });
