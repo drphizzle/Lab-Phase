36c1ab62295bfaf8a15c0f256b3e507e4641faeb
drphizzle <otuphil60@gmail.com>
2025-02-11 04:30:47 +0000

    adding changes

--- menu.js
+++ menu.js
@@ -14,3 +14,29 @@ document.querySelectorAll('.nav-menu a').forEach(link => {
     });
 
 });
+// Contact form interaction
+
+// Generate a random 6-character code
+function generateCode() {
+    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXZY0123456789";
+    let code ="";
+    for (let i = 0; i < 6; i++ ) {
+        code += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return code;
+}
+
+// Display the generated code in the input field
+let codeInput = document.getElementById('generatedCode');
+codeInput.value = generateCode();
+
+// Form submission validation
+document.getElementById('mainForm').addEventListener("submit", function(event) {
+    let userCode = document.getElementById('userCode').value;
+    if (userCode !== codeInput.value) {
+        event.preventDefault();
+        document.getElementById('message').innerText = "incorrect code. Please try again.";
+    } else {
+        alert ("Code is correct! Form submitted.")[;]
+    }
+});
